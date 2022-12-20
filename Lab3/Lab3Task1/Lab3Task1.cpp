e1a9ec6919371a34bc5c33784b245b8f21a765b2
PapievaMary <113173328+PapievaMary@users.noreply.github.com>
2022-12-20 15:34:23 +0300

    Lab3

---
+++ Lab3/Lab3Task1/Lab3Task1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a = 11;
+    int* pointer = &a;
+    cout << "Адрес указателя = " << pointer << endl;
+    cout << "Значение, которое хранится по этому адресу = " << *pointer << endl;
+    *pointer = 20;
+    cout << "Новое значение переменной = " << a << endl;
+    /*****/
+    int arr[] = { 1,2,3,4 };
+    cout << "Вывод всех элементов массива, используя указатель: \n";
+    for (int i = 0; i < 4; i++) {
+        cout << *(arr + i) << " ";
+    }
+    cout << "\n";
+    /*****/
+    int x = 5;
+    int* const p = &x;
+    cout << "Значение переменной x = " << x << "\n";
+    cout << "Адрес переменной = " << p << "\n";
+    *p = 55;
+    cout << "Новое значение переменной x = " << x << "\n";
+    cout << "Адрес переменной = " << p << "\n";
+}
---
+++ Lab3/Lab3Task2/Lab3Task2.cpp
@@ -0,0 +1,58 @@
+﻿// Lab3Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+using namespace std;
+
+void summ(int* begin, int* end)
+{
+	int summ = 0;
+	for (int* p = begin; p < end; p++)
+	{
+		summ = summ + *p;
+	}
+	cout << "Сумма всех элементов массива = " << summ << "\n";
+}
+
+int add(int a, int b) { return a + b; }
+
+int sub(int a, int b) { return a - b; }
+
+int(*choice(char c))(int, int)
+{
+	switch (c)
+	{
+	case '+':
+		return add;
+		break;
+
+	case '-':
+		return sub;
+		break;
+
+	default:
+		return nullptr;
+		break;
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "");
+	int array[] = { 1,2,3,4,5 };
+	int length = sizeof(array) / sizeof(*array);
+	cout << "Массив = ";
+	for (int i = 0; i < 5; i++) {
+		cout << *(array + i) << " ";
+	}
+	cout << endl;
+
+	summ(array, &array[length]);
+	cout << "Сумма = " << add(1, 2) << "\n";
+	cout << "Разность = " << sub(3, 1) << "\n";
+	cout << choice('+')(2, 3) << " = " << add(2, 3) << endl;
+	cout << choice('-')(2, 3) << " = " << sub(2, 3) << endl;
+	/*****/
+	float* x = new float;
+	*x = 1.11;
+	cout << *x << endl;
+	delete x;
+}
